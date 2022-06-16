308e8a3fe2637dd9c66adaa2c7ad4b91a0460d37
Ryan Hull <ryan.hull@familysearch.org>
2022-06-16 15:39:06 -0600

    try that

--- index.js
+++ index.js
@@ -1,32 +1,25 @@
 const path = require('path')
 const fs = require('fs')
 const core = require('@actions/core')
-const github = require('@actions/github')
+// const github = require('@actions/github')
 const exec = require('@actions/exec')
 
 const run = async () => {
   try {
-    const context = github.context
+    // const context = github.context
 
-    const myToken = core.getInput('token')
+    // const myToken = core.getInput('token')
     // const pr = core.getInput('pr')
     const files = core.getInput('files').replace(' ', '').split(',')
 
-    const octokit = github.getOctokit(myToken)
+    // const octokit = github.getOctokit(myToken)
     const root = process.env.GITHUB_WORKSPACE
-    // const { data: pullRequest } = await octokit.rest.pulls.listFiles({
-    //   ...context.repo,
-    //   pull_number: pr,
-    // });
 
-    // const onlyLocales = pullRequest.every((file) => {
-    //   return file.filename.includes('locales')
-    // })
-    // console.log(`was locales only`, onlyLocales)
-    // if (onlyLocales) {
-
-    // }
-    const filesChanged = await exec.exec('git', ['diff', 'origin/master', '--name-only'])
+    const filesChanged = await exec.exec('git', [
+      'diff',
+      'origin/master',
+      '--line-prefix=`git rev-parse --show-toplevel`',
+    ])
     console.log('changed', filesChanged)
 
     const parser = {
@@ -41,10 +34,6 @@ const run = async () => {
 
       const content = parser.read(buffer)
 
-      // if (content.version === version) {
-      //   core.info(`  - ${file}: Skip since equal versions`)
-      //   return change
-      // }
       const newVersion = content.version
         .split('.')
         .map((num, i) => {
@@ -74,7 +63,7 @@ const run = async () => {
       'user.email',
       '41898282+github-actions[bot]@users.noreply.github.com',
     ])
-    await exec.exec('git', ['commit', '-m', `bump version`])
+    await exec.exec('git', ['commit', '-am', `bump version`])
     await exec.exec('git', ['push'])
   } catch (error) {
     core.setFailed(error.message)
